c601891365661bd60b99ed106b90f532aae8ddeb
IT24100738 <it24100738@my.sliit.lk>
2025-09-05 20:30:50 +0530

    Add files via upload

---
+++ Lab Sheet 06/lab6ps.R
@@ -0,0 +1,15 @@
+setwd("C:\\Users\\dhanusha\\Desktop\\pslab6")
+
+#1
+# I.  n = 50, p = 0.85
+
+##II
+pbinom(47, 50, 0.85)
+
+#2
+#I. number of calls per hour
+
+#II. Poisson Distribution -> lambda=12
+
+#III.
+dpois(15, 5)
